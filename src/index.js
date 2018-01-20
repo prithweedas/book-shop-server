eb7369459d3eb607e1bf997db7d5b304000ac690
Prithwee Das <prithweedas123@gmail.com>
2018-01-21 02:46:05 +0530

    basic hello world express app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from "express"
 import bodyParser from "body-parser"
 
-const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
-  res.send("Hello Books")
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.post("/", (req, res) => {
+  res.json(req.body)
 })
 
 app.listen(PORT, err => {
