1588d3b4a980d5a640e0fb709fb4b8fbdf3abb6c
Prithwee Das <prithweedas123@gmail.com>
2018-01-21 19:05:05 +0530

    best practice : Don't repeat yourself ;)

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import _ from "lodash";
 
 import User from "../models/user";
-import throwNewHttpError from '../common/throwNewHttpError';
-import jwtHelper from '../common/jwtHelper';
+import throwNewHttpError from "../common/throwNewHttpError";
+import jwtHelper from "../common/jwtHelper";
 
 const router = express.Router();
 
@@ -38,36 +39,35 @@ router.post("/register", async (req, res, next) => {
     console.log(result);
     res.status(201).json({
       ok: true,
-      user: result
+      user: _.pick(result, ["_id", "email", "name", "college"])
     });
   } catch (error) {
     next(error);
   }
 });
 
-router.post('/login', async (req, res, next) => {
+router.post("/login", async (req, res, next) => {
   const { email, password } = req.body;
 
   try {
     const user = await User.findByCredencials(email, password);
-    if (!user)
-      next(throwNewHttpError("wrong email or password", 400));
+    if (!user) next(throwNewHttpError("wrong email or password", 400));
 
     const result = jwtHelper.generateToken(user);
     const refreshToken = jwtHelper.generateRefreshToken(user);
-    res.header('token', result.token)
-      .header('token-expiresBy', result.TokenExpiresBy)
-      .header('refreshToken', refreshToken)
+    res
+      .header({
+        token: result.token,
+        "token-expiresBy": result.TokenExpiresBy,
+        refreshToken: refreshToken
+      })
       .send({
         ok: true,
-        user
+        user: _.pick(user, ["_id", "email", "name", "college"])
       });
-  }
-  catch (error) {
+  } catch (error) {
     next(error);
   }
-
 });
-
 
 export default router;
