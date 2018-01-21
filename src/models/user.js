235430c14e52bb58ee72327b4a972129756e2f03
Ritwick Dey <ritwickdey@outlook.com>
2018-01-21 16:46:29 +0530

    unique email & user friendly message

--- src/models/user.js
+++ src/models/user.js
@@ -12,7 +12,13 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true,
+    trim: true,
+    validate: {
+      validator: v => new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).exec(v),
+      message: '{VALUE} is not a valid Email!'
+    },
   },
   college: {
     type: String,
@@ -23,5 +29,19 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 });
+
+userSchema.post('save', (error, doc, next) => {
+  if (!error) next();
+
+  if (error.name === "BulkWriteError" && error.code === 11000) {
+    error.status = 400;
+    error.message = 'email address is already in use'
+    next(error);
+  }
+  else
+    next(error);
+
+});
+
 
 export default mongoose.model("User", userSchema);
