dc1179774a156ae74a942d0bc82c15c1dfa2ff8e
Prithwee Das <prithweedas123@gmail.com>
2018-01-21 21:18:56 +0530

    item patch method upated

--- src/routes/items.js
+++ src/routes/items.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 
 import Item from "../models/item";
-import authenticate from '../common/authenticate';
+import authenticate from "../common/authenticate";
 
 const router = express.Router();
 
@@ -28,12 +28,12 @@ router.get("/:id", async (req, res, next) => {
       item: result
     });
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 
 router.post("/", authenticate, async (req, res, next) => {
-  const { name, price, publishYear, author, description, userId } = req.body;
+  const { name, price, publishYear, author, description, owner } = req.body;
   const item = new Item({
     _id: mongoose.Types.ObjectId(),
     name,
@@ -41,7 +41,7 @@ router.post("/", authenticate, async (req, res, next) => {
     price,
     author,
     description,
-    owner : userId
+    owner
   });
   console.log(userId);
   try {
@@ -57,16 +57,11 @@ router.post("/", authenticate, async (req, res, next) => {
 
 router.patch("/:id", authenticate, async (req, res, next) => {
   const id = req.params.id;
-  const updateOptions = {};
-  Object.keys(req.body).forEach(key => (updateOptions[key] = req.body[key]));
+  const { sold } = req.body;
   try {
-    const result = await Item.update(
-      { _id: id },
-      { $set: updateOptions }
-    ).exec();
+    await Item.update({ _id: id }, { $set: { sold } }).exec();
     res.status(200).json({ ok: true });
-  }
-  catch (error) {
+  } catch (error) {
     next(error);
   }
 });
