f0d1264ad902eb0bfc5a6921fffb91e0d93150f3
Ritwick Dey <ritwickdey@outlook.com>
2018-01-22 15:20:56 +0530

    Opps! Access-Control-Expose-Headers is also required for CROS. Otherwise will not expose it :smile:

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ app.use(morgan("dev"));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Expose-Headers', 'token, refreshToken, token-expiresby');
 
   if (req.method === 'OPTIONS') {
       res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE');
