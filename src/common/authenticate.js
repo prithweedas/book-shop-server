eb1684b523089eb93c969e244ea801cf9e123f3e
Ritwick Dey <ritwickdey@outlook.com>
2018-02-18 23:12:56 +0530

    refactored authenticate & setAuthentication

--- src/common/authenticate.js
+++ src/common/authenticate.js
@@ -5,31 +5,19 @@ const authenticate = async (req, res, next) => {
   const token = req.header("token");
   const refreshToken = req.header("refreshToken");
 
-  if (!token) return next(throwNewHttpError("Token is not provided", 401));
+  const authResult = await checkAuthentication(token, refreshToken);
 
-  let decodedToken = jwtHelper.getDecodedTokenIfValid(token);
-  const decodedRefreshToken = jwtHelper.getDecodedTokenIfValid(refreshToken);
-
-  if (!decodedToken && !decodedRefreshToken)
-    return next(throwNewHttpError("Token expried", 401));
-
-  if (!decodedToken) {
-    const result = await jwtHelper.generateTokenByUserId(
-      decodedRefreshToken.userId
-    );
-    const refreshToken = await jwtHelper.generateRefreshToken(
-      decodedRefreshToken.userId
-    );
-    res.set({
-      token: result.token,
-      "token-expiresBy": result.TokenExpiresBy,
-      refreshToken: refreshToken
-    });
-    decodedToken = jwtHelper.getDecodedTokenIfValid(result.token);
+  if (authResult.error) {
+    return next(throwNewHttpError(authResult.error, 401));
   }
 
-  req.userId = decodedToken.userId;
-  req.userEmail = decodedToken.email;
+  res.set({
+    token: authResult.token,
+    refreshToken: authResult.refreshToken
+  });
+
+  req.userId = authResult.userId;
+  req.userEmail = authResult.userEmail;
 
   next();
 };
@@ -38,33 +26,53 @@ export const setAuthentication = async (req, res, next) => {
   const token = req.header("token");
   const refreshToken = req.header("refreshToken");
 
-  if (!token) return next();
+  const authResult = await checkAuthentication(token, refreshToken);
+
+  if (authResult.error) {
+    return next();
+  }
+
+  res.set({
+    token: authResult.token,
+    refreshToken: authResult.refreshToken
+  });
+
+  req.userId = authResult.userId;
+  req.userEmail = authResult.userEmail;
+
+  next();
+};
+
+const checkAuthentication = async (token, refreshToken) => {
+  if (!token) return { error: "Token is not provided" };
 
   let decodedToken = jwtHelper.getDecodedTokenIfValid(token);
   const decodedRefreshToken = jwtHelper.getDecodedTokenIfValid(refreshToken);
 
-  if (!decodedToken && !decodedRefreshToken)
-    return next();
+  if (!decodedToken && !decodedRefreshToken) return { error: "Token expried" };
 
   if (!decodedToken) {
-    const result = await jwtHelper.generateTokenByUserId(
+    const tokenResult = await jwtHelper.generateTokenByUserId(
       decodedRefreshToken.userId
     );
-    const refreshToken = await jwtHelper.generateRefreshToken(
+
+    //updating paramenter variables
+    token = tokenResult.token;
+    refreshToken = await jwtHelper.generateRefreshToken(
       decodedRefreshToken.userId
     );
-    res.set({
-      token: result.token,
-      "token-expiresBy": result.TokenExpiresBy,
-      refreshToken: refreshToken
-    });
-    decodedToken = jwtHelper.getDecodedTokenIfValid(result.token);
+
+    decodedToken = jwtHelper.getDecodedTokenIfValid(tokenResult.token);
   }
 
-  req.userId = decodedToken.userId;
-  req.userEmail = decodedToken.email;
-
-  next();
+  console.log("doooooooooooooo");
+  return {
+    ok: true,
+    token,
+    refreshToken,
+    userId: decodedToken.userId,
+    userEmail: decodedToken.email
+  };
 };
 
 export default authenticate;
