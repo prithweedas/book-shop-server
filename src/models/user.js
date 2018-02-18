e244e5e037ac0bc7402d3fecf4356e9a092c84d4
Ritwick Dey <ritwickdey@outlook.com>
2018-02-18 21:30:02 +0530

    small mistake :smile:

--- src/models/user.js
+++ src/models/user.js
@@ -66,11 +66,6 @@ userSchema.methods.toJSON = function () {
   return user;
 }
 
-userSchema.methods.toJSON = function() {
-  const user = this.toObject();
-  delete user.__v;
-  return user;
-};
 
 const User = mongoose.model("User", userSchema);
 export default User;
